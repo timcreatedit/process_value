6a5690c3360b7a5c60ff00174ebbcc1b9bf6fcb8
Tim <hi@timcreated.it>
2023-07-19 18:15:33 +0200

    feat: initial release (#1)
    * 100% coverage
    
    * add README
    
    * fixed workflows
    
    * updated workflows

--- lib/src/process_value.dart
+++ lib/src/process_value.dart
@@ -9,6 +9,16 @@ import 'package:meta/meta.dart';
 sealed class ProcessValue<T> {
   /// {@macro process_value}
   const ProcessValue();
+
+  /// {@macro process_data}
+  const factory ProcessValue.data(T value) = ProcessData<T>;
+
+  /// {@macro process_loading}
+  const factory ProcessValue.loading(double progress) = ProcessLoading<T>;
+
+  /// {@macro process_error}
+  const factory ProcessValue.error(Object error, {StackTrace? stackTrace}) =
+      ProcessError<T>;
 }
 
 /// {@template process_data}
@@ -83,7 +93,7 @@ class ProcessError<T> extends ProcessValue<T> {
   final StackTrace? stackTrace;
 
   @override
-  String toString() => 'ProcessError<$T>($error, $stackTrace)';
+  String toString() => 'ProcessError<$T>($error)';
 
   @override
   bool operator ==(Object other) =>
--- test/src/process_value_test.dart
+++ test/src/process_value_test.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:process_value/process_value.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group(ProcessValue, () {
+  group("ProcessValue", () {
     test("sealed union has three types", () async {
       // This test won't compile if the shape of the union changes.
       const ProcessValue<int> sut = ProcessData(42);
@@ -16,24 +18,80 @@ void main() {
       }
       expect(true, true);
     });
+
+    group(".data", () {
+      test("creates a ProcessData", () async {
+        expect(ProcessValue.data(42), ProcessData(42));
+      });
+    });
+
+    group(".loading", () {
+      test("creates a ProcessLoading", () async {
+        expect(ProcessValue<int>.loading(0.5), ProcessLoading<int>(0.5));
+      });
+    });
+
+    group(".error", () {
+      test("creates a ProcessError", () async {
+        expect(ProcessValue<int>.error("oops"), ProcessError<int>("oops"));
+      });
+    });
   });
-  group(ProcessData, () {
+  group("ProcessData", () {
     test("value equality", () async {
       expect(const ProcessData(42), const ProcessData(42));
       expect(const ProcessData(42), isNot(const ProcessData(43)));
     });
-  });
 
-  group(ProcessLoading, () {
-    test("value equality", () async {
-      expect(const ProcessLoading<int>(0.5), const ProcessLoading<int>(0.5));
+    test("toString", () async {
+      expect(const ProcessData(42).toString(), "ProcessData<int>(42)");
+    });
+
+    test("hashCode", () async {
+      expect(const ProcessData(42).hashCode, const ProcessData(42).hashCode);
     });
   });
 
-  group(ProcessError, () {
+  group("ProcessLoading", () {
+    test("value equality", () async {
+      expect(ProcessLoading<int>(0.5), ProcessLoading<int>(0.5));
+    });
+
+    test("toString", () async {
+      expect(
+        const ProcessLoading<int>(0.5).toString(),
+        "ProcessLoading<int>(0.5)",
+      );
+    });
+
+    test("hashCode", () async {
+      expect(
+        const ProcessLoading<int>(0.5).hashCode,
+        const ProcessLoading<int>(0.5).hashCode,
+      );
+    });
+  });
+
+  group("ProcessError", () {
     test("value equality", () async {
       final exception = Exception();
       expect(ProcessError<int>(exception), ProcessError<int>(exception));
+    });
+
+    test("toString", () async {
+      final exception = Exception();
+      expect(
+        ProcessError<int>(exception).toString(),
+        "ProcessError<int>($exception)",
+      );
+    });
+
+    test("hashCode", () async {
+      final exception = Exception();
+      expect(
+        ProcessError<int>(exception).hashCode,
+        ProcessError<int>(exception).hashCode,
+      );
     });
   });
 }
